865396371c4266b0bc61b753eccc5c218485f608
kbh9974@naver.com <kbh9974@naver.com>
2025-03-13 11:44:14 +0900

    modified:   unitree_guide/include/common/unitreeRobot.h

--- unitree_guide/include/common/unitreeRobot.h
+++ unitree_guide/include/common/unitreeRobot.h
@@ -1,6 +1,27 @@
 /**********************************************************************
  Copyright (c) 2020-2023, Unitree Robotics.Co.Ltd. All rights reserved.
 ***********************************************************************/
+
+/**********************************************************************
+Release v0.1(250313)
+설명: Unitree의 4족 로봇을 위한 클래스 정의
+함수
+- QuadrupedRobot class: 로봇의 다리 운동과 위치 추적을 위한 클래스 정의
+- getX, getVecXP: 로봇의 현재 위치 및 자세를 LowlevelState 객체를 통해 반환
+- getQ, getQd, getTau: 역기구학 계산을 수행하며, 주어진 발의 위치나 속도에 대응하여 관절 각도, 각속도 반환. 관절 각도를 이용하여 발의 힘 반환
+- getFootPosition: 로봇의 다리 위치 계산
+- getFootVelocity: 로봇의 다리 속도 계산
+- getFeet2BPositions: 로봇의 다리 위치를 몸체 좌표계 기준으로 반환
+- getFeet2BVelocities: 로봇의 다리 속도를 몸체 좌표계 기준으로 반환
+- getJaco: 다리에 대한 자코비안 행렬 반환
+- getRobVelLimitX, Y, Yaw: 로봇의 이동 및 회전 속도 제한 설정
+- getFeetPosIdeal, getRobMass, getPcb, getRobInertial: 로봇의 이상적인 발 위치, 질량, 중심 위치, 관성 행렬 반환
+- A1Robot, Go1Robot class: 특정 로봇 모델에 맞게 구현된 클래스 
+
+Release v0.2(250313)
+- B1Robot class 추가, B1Robot 함수 정의 필요
+***********************************************************************/
+
 #ifndef UNITREEROBOT_H
 #define UNITREEROBOT_H
 
@@ -56,6 +77,12 @@ class Go1Robot : public QuadrupedRobot{
 public:
     Go1Robot();
     ~Go1Robot(){};
+};
+
+class B1Robot : public QuadrupedRobot{
+public:
+    B1Robot();
+    ~B1Robot(){};
 };
 
 #endif  // UNITREEROBOT_H
